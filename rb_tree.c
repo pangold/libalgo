5d98c6228d30aa43a2a9032e3a5a8974b34cdbfd
pandora <pangold@163.com>
2020-08-11 01:18:34 +0800

    update rb_tree

--- rb_tree.c
+++ rb_tree.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include "stack.h"
 #include "rb_tree.h"
 
 #define RED   0
@@ -20,13 +21,199 @@ typedef struct rb_tree {
     struct rb_tree_node *root;
     struct rb_tree_node *nil;
     int   (*compare)(void *, void *);
-    int   key_sizeof_type;
-    int   value_sizeof_type;
+    int     key_sizeof_type;
+    int     value_sizeof_type;
     void* (*key_alloc)(void *);
     void* (*value_alloc)(void *);
     void  (*key_free)(void *);
     void  (*value_free)(void *);
 } rb_tree_t;
+
+static void rb_tree_node_pre_order_0(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    cb(root, arg);
+    rb_tree_node_pre_order_0(root->left, cb, arg);
+    rb_tree_node_pre_order_0(root->right, cb, arg);
+}
+
+static void rb_tree_node_pre_order_1(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    rb_tree_node_t *node;
+    stack_t *stack = stack_alloc();
+    stack_push(stack, root);
+    while (stack_size(stack) != 0) {
+        node = (rb_tree_node_t *)stack_pop(stack);
+        cb(node, arg);
+        if (node->right) stack_push(stack, node->right);
+        if (node->left) stack_push(stack, node->left);
+    }
+    stack_free(stack, NULL);
+}
+
+static void rb_tree_node_pre_order_2(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    rb_tree_node_t *pre, *node;
+    for (node = root; node; ) {
+        if (!node->left) {
+            cb(node, arg);
+            node = node->right;
+            continue;
+        }
+        pre = node->left;
+        while (pre->right && pre->right != node) pre = pre->right;
+        if (!pre->right) {
+            pre->right = node;
+            cb(node, arg);
+            node = node->left;
+            continue;
+        }
+        pre->right = NULL;
+        node = node->right;
+    }
+}
+
+static void rb_tree_node_in_order_0(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    rb_tree_node_in_order_0(root->left, cb, arg);
+    cb(root, arg);
+    rb_tree_node_in_order_0(root->right, cb, arg);
+}
+
+static void rb_tree_node_in_order_1(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    stack_t *stack = stack_alloc();
+    rb_tree_node_t *node = root;
+    while (node || stack_size(stack) != 0) {
+        if (!node) {
+            node = (rb_tree_node_t *)stack_pop(stack);
+            cb(node, arg);
+            node = node->right;
+            continue;
+        }
+        stack_push(stack, node);
+        node = node->left;
+    }
+    stack_free(stack, NULL);
+}
+
+static void rb_tree_node_in_order_2(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    rb_tree_node_t *pre, *node;
+    for (node = root; node; ) {
+        if (!node->left) {
+            cb(node, arg);
+            node = node->right;
+            continue;
+        }
+        pre = node->left;
+        while (pre->right && pre->right != node) pre = pre->right;
+        if (!pre->right) {
+            pre->right = NULL;
+            cb(node, arg);
+            node = node->right;
+            continue;
+        }
+        pre->right = node;
+        node = node->left;
+    }
+}
+
+static void rb_tree_node_post_order_0(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    rb_tree_node_post_order_0(root->left, cb, arg);
+    rb_tree_node_post_order_0(root->right, cb, arg);
+    cb(root, arg);
+}
+
+static void rb_tree_node_post_order_1(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    rb_tree_node_t *pre, *node;
+    stack_t *stack = stack_alloc();
+    stack_t *stack2 = stack_alloc();
+    stack_push(stack, root);
+    while (stack_size(stack) != 0) {
+        node = (rb_tree_node_t *)stack_pop(stack);
+        stack_push(stack2, node);
+        if (node->left) stack_push(stack, node->left);
+        if (node->right) stack_push(stack, node->right);
+    }
+    while (stack_size(stack2) != 0) {
+        node = (rb_tree_node_t *)stack_pop(stack2);
+        cb(node, arg);
+    }
+}
+
+static void rb_tree_node_reverse(rb_tree_node_t *n1, rb_tree_node_t *n2)
+{
+    if (n1 == n2) return;
+    rb_tree_node_t *x = n1;
+    rb_tree_node_t *y = n1->right;
+    while (1) {
+        rb_tree_node_t *tmp = y->right;
+        y->right = x;
+        x = y;
+        y = tmp;
+        if (x == n2) break;
+    }
+}
+
+static void rb_tree_node_reverse_cb(rb_tree_node_t *n1, rb_tree_node_t *n2, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    rb_tree_node_reverse(n1, n2);
+    rb_tree_node_t *node = n2;
+    while (1) {
+        cb(node, arg);
+        if (node == n1) break;
+        node = node->right;
+    }
+    rb_tree_node_reverse(n2, n1);
+}
+
+static void rb_tree_node_post_order_2(rb_tree_node_t *root, void(*cb)(rb_tree_node_t *, void *), void *arg)
+{
+    if (!root) return;
+    rb_tree_node_t *pre, *node;
+    rb_tree_node_t *dummy = (rb_tree_node_t *)malloc(sizeof(rb_tree_node_t));
+    dummy->left = root;
+    for (node = dummy; node; ) {
+        if (!node->left) {
+            node = node->right;
+            continue;
+        }
+        pre = node->left;
+        while (pre->right && pre->right != node) pre = pre->right;
+        if (!pre->right) {
+            pre->right = node;
+            node = node->left;
+            continue;
+        }
+        rb_tree_node_reverse_cb(node->left, pre, cb, arg);
+        pre->right = NULL;
+        node = node->right;
+    }
+    free(dummy);
+}
+
+static rb_tree_node_t *rb_tree_node_search(rb_tree_node_t *root, void *key, int(*compare)(void *, void *))
+{
+    if (!root) return NULL;
+    rb_tree_node_t *node;
+    for (node = root;  node; ) {
+        int ret = compare(key, node->key);
+        if (ret == 0) return node;
+        else if (ret > 0) node = node->right;
+        else node = node->left;
+    }
+    return NULL;
+}
 
 /* 
  *
@@ -40,7 +227,7 @@ typedef struct rb_tree {
  *
  *
  */
-static void rb_tree_left_rotate(rb_tree_t *tree, rb_tree_node_t *x)
+static void rb_tree_node_left_rotate(rb_tree_t *tree, rb_tree_node_t *x)
 {
     rb_tree_node_t *y;
     y = x->right;
@@ -71,7 +258,7 @@ static void rb_tree_left_rotate(rb_tree_t *tree, rb_tree_node_t *x)
  *      lx  rx                                rx  ry
  * 
  */
-static void rb_tree_right_rotate(rb_tree_t *tree, rb_tree_node_t *y)
+static void rb_tree_node_right_rotate(rb_tree_t *tree, rb_tree_node_t *y)
 {
     rb_tree_node_t *x;
     x = y->left;
@@ -101,11 +288,11 @@ static void rb_tree_insert_fixup_left(rb_tree_t *tree, rb_tree_node_t **node)
         (*node) = (*node)->parent->parent;
     } else if ((*node) == (*node)->parent->right) {
         (*node) = (*node)->parent;
-        rb_tree_left_rotate(tree, (*node));
+        rb_tree_node_left_rotate(tree, (*node));
     } else {
         (*node)->parent->color = BLACK;
         (*node)->parent->parent->color = RED;
-        rb_tree_right_rotate(tree, (*node)->parent->parent);
+        rb_tree_node_right_rotate(tree, (*node)->parent->parent);
     }
 }
 
@@ -119,11 +306,11 @@ static void rb_tree_insert_fixup_right(rb_tree_t *tree, rb_tree_node_t **node)
         (*node) = (*node)->parent->parent;
     } else if ((*node) == (*node)->parent->left) {
         (*node) = (*node)->parent;
-        rb_tree_right_rotate(tree, (*node));
+        rb_tree_node_right_rotate(tree, (*node));
     } else {
         (*node)->parent->color = BLACK;
         (*node)->parent->parent->color = RED;
-        rb_tree_left_rotate(tree, (*node)->parent->parent);
+        rb_tree_node_left_rotate(tree, (*node)->parent->parent);
     }
 }
 
@@ -142,7 +329,7 @@ static void rb_tree_insert_fixup(rb_tree_t *tree, rb_tree_node_t *node)
 
 static void rb_tree_insert(rb_tree_t *tree, rb_tree_node_t *node)
 {
-    rb_tree_node_t *parent, *x;
+    rb_tree_node_t *x, *parent = NULL;
     for (x = tree->root; x; ) {
         parent = x;
         x = tree->compare(node->key, x->key) ? x->left : x->right;
@@ -165,7 +352,7 @@ static int rb_tree_delete_fixup_left(rb_tree_t *tree, rb_tree_node_t **node, rb_
         // Case 1: x的兄弟w是红色的
         temp->color = BLACK;
         (*parent)->color = RED;
-        rb_tree_left_rotate(tree, (*parent));
+        rb_tree_node_left_rotate(tree, (*parent));
         temp = (*parent)->right;
     }
     if ((!temp->left || temp->left->color == BLACK) &&
@@ -179,14 +366,14 @@ static int rb_tree_delete_fixup_left(rb_tree_t *tree, rb_tree_node_t **node, rb_
             // Case 3: x的兄弟w是黑色的，并且w的左孩子是红色，右孩子为黑色。
             temp->left->color = BLACK;
             temp->color = RED;
-            rb_tree_right_rotate(tree, temp);
+            rb_tree_node_right_rotate(tree, temp);
             temp = (*parent)->right;
         }
         // Case 4: x的兄弟w是黑色的；并且w的右孩子是红色的，左孩子任意颜色。
         temp->color = (*parent)->color;
         (*parent)->color = BLACK;
         temp->right->color = BLACK;
-        rb_tree_left_rotate(tree, (*parent));
+        rb_tree_node_left_rotate(tree, (*parent));
         (*node) = tree->root;
         return 1;
     }
@@ -200,7 +387,7 @@ static int rb_tree_delete_fixup_right(rb_tree_t *tree, rb_tree_node_t **node, rb
         // Case 1: x的兄弟w是红色的  
         temp->color = BLACK;
         (*parent)->color = RED;
-        rb_tree_right_rotate(tree, (*parent));
+        rb_tree_node_right_rotate(tree, (*parent));
         temp = (*parent)->left;
     }
     if ((!temp->left || temp->left->color == BLACK) &&
@@ -214,14 +401,14 @@ static int rb_tree_delete_fixup_right(rb_tree_t *tree, rb_tree_node_t **node, rb
             // Case 3: x的兄弟w是黑色的，并且w的左孩子是红色，右孩子为黑色。
             temp->right->color = BLACK;
             temp->color = RED;
-            rb_tree_left_rotate(tree, temp);
+            rb_tree_node_left_rotate(tree, temp);
             temp = (*parent)->left;
         }
         // Case 4: x的兄弟w是黑色的；并且w的右孩子是红色的，左孩子任意颜色。
         temp->color = (*parent)->color;
         (*parent)->color = BLACK;
         temp->left->color = BLACK;
-        rb_tree_right_rotate(tree, (*parent));
+        rb_tree_node_right_rotate(tree, (*parent));
         (*node) = tree->root;
         return 1;
     }
@@ -334,10 +521,11 @@ static rb_tree_node_t *rb_tree_node_alloc(void *key, void *value)
     return node;
 }
 
-static void rb_tree_node_free(rb_tree_node_t *node, void(*kfree)(void *), void (*vfree)(void *))
+static void rb_tree_node_free(rb_tree_node_t *node, void *arg)
 {
-    if (kfree && node->key) kfree(node->key);
-    if (vfree && node->value) vfree(node->value);
+    rb_tree_t *tree = (rb_tree_t *)arg;
+    if (tree && tree->key_free && node->key) tree->key_free(node->key);
+    if (tree && tree->value_free && node->value) tree->value_free(node->value);
     free(node);
 }
 
@@ -358,12 +546,17 @@ rb_tree_t *rb_tree_alloc()
 
 void rb_tree_clear(rb_tree_t *tree)
 {
-    // foreach and free each nodes
+    if (!tree) return;
+    rb_tree_node_post_order_0(tree->root, rb_tree_node_free, tree);
+    tree->root = NULL;
 }
 
 void rb_tree_free(rb_tree_t *tree)
 {
-    if (tree->key_free);
+    if (!tree) return;
+    rb_tree_node_post_order_0(tree->root, rb_tree_node_free, tree);
+    free(tree->nil);
+    free(tree);
 }
 
 void rb_tree_set_key_alloc(rb_tree_t *tree, void*(*alloc)(void *))
@@ -399,24 +592,39 @@ void rb_tree_push(rb_tree_t *tree, void *key, void *value)
 
 void rb_tree_pop(rb_tree_t *tree, void *key)
 {
-    rb_tree_node_t *node = rb_tree_search(tree, key);
+    rb_tree_node_t *node = rb_tree_node_search(tree->root, key, tree->compare);
     rb_tree_delete(tree, node);
 }
 
 void *rb_tree_find(rb_tree_t *tree, void *key)
 {
-    rb_tree_node_t *node = rb_tree_search(tree, key);
+    rb_tree_node_t *node = rb_tree_node_search(tree->root, key, tree->compare);
     return node->value;
 }
 
 void *rb_tree_at(rb_tree_t *tree, size_t n)
 {
-
+    return NULL;
 }
 
-void rb_tree_foreach(rb_tree_t *tree, void(void *, void *, void *), void *arg)
-{
+struct rb_tree_foreach_user_cb {
+    void(*func)(void *, void *, void *);
+    void *arg;
+};
 
+static void rb_tree_foreach_cb(rb_tree_node_t *node, void *arg)
+{
+    struct rb_tree_foreach_user_cb *ucb;
+    ucb = (struct rb_tree_foreach_user_cb *)arg;
+    ucb->func(node->key, node->value, ucb->arg);
+}
+
+void rb_tree_foreach(rb_tree_t *tree, void(*cb)(void *, void *, void *), void *arg)
+{
+    struct rb_tree_foreach_user_cb ucb;
+    ucb.func = cb;
+    ucb.arg = arg;
+    rb_tree_node_in_order_2(tree->root, rb_tree_foreach_cb, &ucb);
 }
 
 #if defined(ENABLE_RBTREE_TEST)
@@ -433,6 +641,10 @@ static void rb_tree_kv_int_free(void *src)
 static int rb_tree_int_less_than(void *i1, void *i2)
 {
     return *(int *)i1 < *(int *)i2;
+}
+static void rb_tree_print(void *key, void *value, void *arg)
+{
+    printf("key = %d, value = %d\n", *(int *)key, *(int *)value);
 }
 void rb_tree_test()
 {
@@ -457,7 +669,9 @@ void rb_tree_test()
     rb_tree_push(tree, &key[8], &value[8]);
     rb_tree_push(tree, &key[9], &value[9]);
 
-    rb_tree_pop(tree, &key[0]);
+    rb_tree_foreach(tree, rb_tree_print, NULL);
+
+    // rb_tree_pop(tree, &key[0]);
 
     rb_tree_free(tree);
 }
